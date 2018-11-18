db66b9ef63c28bdf6b2e9db57aac26393ede7906
ivan <ivan.esguerra@arsokos.com>
2018-11-18 22:59:48 +0800

    added new projects and added animation for arrow

--- dist/js/scrollTop.js
+++ dist/js/scrollTop.js
@@ -1,8 +1,13 @@
 window.onscroll = () => {
+    const arrow = document.getElementById('arrow-up');
     if (parseInt(window.pageYOffset) >= 900) {
-        document.getElementById('arrow-up').style.opacity = '1';
+        arrow.classList.remove('fadeOut');
+        arrow.style.opacity = '1';   
+        arrow.classList.add('fadeIn');
     } else {
-        document.getElementById('arrow-up').style.opacity = '0';   
+        arrow.classList.remove('fadeIn');
+        arrow.style.opacity = '0';   
+        arrow.classList.add('fadeOut');
     }
 };
 
